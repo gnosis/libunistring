a5e5a3068605f089ad6cca005052b6ff3ddebca5
Bruno Haible <bruno@clisp.org>
2009-05-01 19:02:41 +0200

    Clarify which gnulib version to use.

--- version.sh
+++ version.sh
@@ -1,3 +1,6 @@
 # Version number and release date.
 VERSION_NUMBER=0.9
 RELEASE_DATE=2009-04-26      # in "date +%Y-%m-%d" format
+
+# Version of gnulib that was used in this release.
+GNULIB_GIT_COMMIT=13a006f97fca894168e4c1aedfa780f83717c78c

76de81684dc4a3c68d5a9d34c022bc324f6382f7
Bruno Haible <bruno@clisp.org>
2009-07-01 02:10:17 +0200

    Prepare for version 0.9.1.

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 # Version number and release date.
-VERSION_NUMBER=0.9
-RELEASE_DATE=2009-04-26      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.9.1
+RELEASE_DATE=2009-06-30      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
-GNULIB_GIT_COMMIT=13a006f97fca894168e4c1aedfa780f83717c78c
+GNULIB_GIT_COMMIT=e43d583475dc364a0816df054471e6bc4bb75106

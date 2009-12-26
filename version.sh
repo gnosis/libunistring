e89eb9a79e3e46419ee593a3eac246cd848a3f60
Bruno Haible <bruno@clisp.org>
2009-12-26 20:03:42 +0100

    Use a newer gnulib.

--- version.sh
+++ version.sh
@@ -3,4 +3,4 @@ VERSION_NUMBER=0.9.2
 RELEASE_DATE=2009-12-26      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
-GNULIB_GIT_COMMIT=a93454a87ee0ea0bcd92ea55c1eb87330119d438
+GNULIB_GIT_COMMIT=65887d1054c42a1650d668de2065afa7a7e7fc36

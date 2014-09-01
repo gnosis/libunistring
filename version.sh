0524792f4d2dfcbb798c817477bf737e5a044db8
Daiki Ueno <ueno@gnu.org>
2014-09-01 10:24:25 +0900

    Prepare for release 0.9.4.

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 # Version number and release date.
-VERSION_NUMBER=0.9.3
-RELEASE_DATE=2010-05-02      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.9.4
+RELEASE_DATE=2014-08-12      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
-GNULIB_GIT_COMMIT=28f3679a51f29cdaa8cb6c8211fa6ecaad3a852c
+GNULIB_GIT_COMMIT=a6c54be167bd5be41013a254b4e6ba840420a1d0

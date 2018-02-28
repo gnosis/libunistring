02f0abdd3359549627352f6dbe7047e8fe7fde4c
Daiki Ueno <ueno@gnu.org>
2018-02-28 16:48:16 +0100

    Prepare for 0.9.9 release

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 # Version number and release date.
-VERSION_NUMBER=0.9.8
-RELEASE_DATE=2017-11-30      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=0.9.9
+RELEASE_DATE=2018-02-28      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
-GNULIB_GIT_COMMIT=82edd0e68b9e88fb24c1d32ad815083f66fae1ef
+GNULIB_GIT_COMMIT=e6aa578643640d094cbc4c0985066266203c1535
